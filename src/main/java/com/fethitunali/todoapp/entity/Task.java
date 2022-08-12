af1b37609285547d74258fb55e963b92a9a3c49d
tunalime <mfethi96@gmail.com>
2022-08-12 15:21:12 +0300

    entity updated

--- src/main/java/com/fethitunali/todoapp/entity/Task.java
+++ src/main/java/com/fethitunali/todoapp/entity/Task.java
@@ -1,7 +1,35 @@
 package com.fethitunali.todoapp.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Column
+    private Date createdAt;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/com/fethitunali/todoapp/entity/User.java
+++ src/main/java/com/fethitunali/todoapp/entity/User.java
@@ -1,7 +1,34 @@
 package com.fethitunali.todoapp.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private Date createdAt;
 }
