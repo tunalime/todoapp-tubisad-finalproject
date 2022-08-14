731967c75a4fbc681816a23e2dad5370985d9e0e
tunalime <mfethi96@gmail.com>
2022-08-14 19:48:55 +0300

    Repository annotation added.

--- src/main/java/com/fethitunali/todoapp/dao/TaskRepository.java
+++ src/main/java/com/fethitunali/todoapp/dao/TaskRepository.java
@@ -2,6 +2,8 @@ package com.fethitunali.todoapp.dao;
 
 import com.fethitunali.todoapp.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
