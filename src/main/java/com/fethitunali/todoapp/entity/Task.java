46e22cc935d2d68e97386df1a07813531fd0187c
tunalime <mfethi96@gmail.com>
2022-08-15 05:10:33 +0300

    Entity revised.

--- src/main/java/com/fethitunali/todoapp/entity/Task.java
+++ src/main/java/com/fethitunali/todoapp/entity/Task.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
 @Getter
