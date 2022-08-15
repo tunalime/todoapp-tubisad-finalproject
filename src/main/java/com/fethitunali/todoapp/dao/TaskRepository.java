3a781602390505f3f413f1f6b4062f81e89e4f5b
tunalime <mfethi96@gmail.com>
2022-08-15 05:12:34 +0300

    TaskRepository updated.

--- src/main/java/com/fethitunali/todoapp/dao/TaskRepository.java
+++ src/main/java/com/fethitunali/todoapp/dao/TaskRepository.java
@@ -9,5 +9,7 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> getAllByStatus(String status);
+    List<Task> getAllByStatusOrderById(String status);
+
+    List<Task> getAllByOrderByIdAsc();
 }
